a8382919f8bf2be2beee9950e32a49391b0d9095
Natalya Lebedeva <natashaswan.ca@gmail.com>
2023-01-04 13:41:23 -0800

    add counter functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  //let isMounted: boolean = true;
+
+  useEffect(()=>{
+      const interval = setInterval(()=>{setCounter(prev=> prev+1)}, 1000);
+    
+    return ()=>{
+      clearInterval(interval)
+    }
+  }, []);
+
   return (
     <div className="App">
       <h1>Timer</h1>
-      
+      <div>{counter}</div>
     </div>
   );
 }
