d4b6fe2d1d906246dd8701cf6c6cfc9b7235a1fa
Natalya Lebedeva <natashaswan.ca@gmail.com>
2023-01-04 15:08:47 -0800

    add tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, {useEffect, useState, useCallback, useRef} from 'react';
 import './App.css';
 
 const Timer = () => {
 
   const [counter, setCounter] = useState(0);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const onMouseOver = useCallback(()=>{setShowTooltip(true)}, []);
+  const onMouseOut = useCallback(()=>{setShowTooltip(false)}, []);
   
   useEffect(()=>{
     const interval = setInterval(()=>{setCounter(prev=> prev+1)}, 1000);
-    
+    tooltipRef.current?.addEventListener('mouseover', onMouseOver);
+    tooltipRef.current?.addEventListener('mouseout', onMouseOut);
     return ()=>{
       clearInterval(interval)
     }
@@ -17,6 +22,7 @@ const Timer = () => {
     <div >
     <h1>Timer</h1>
     <div>{counter}</div>
+    <div ref={tooltipRef}>Tooltip{showTooltip && <div >future counter: {counter +1}</div>}</div>
   </div>
   )
 }
